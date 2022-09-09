ef73d04094414492a71487972ce5f76f47673327
issacamara <isskamara@live.fr>
2022-09-09 16:07:56 +0000

    changes to web app and train

--- train.py
+++ train.py
@@ -11,49 +11,69 @@ import pandas as pd
 import numpy as np
 import yfinance as yf
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler 
+import math
+from tensorflow import keras
+from tensorflow.keras.layers import LSTM, Dropout, Dense
 
 
-stock_list = ['7105.KL']
-data = yf.download(stock_list, start="2018-01-01", end="2022-01-01", 
-                   interval="1d")
-data.head()
-
-training = data.iloc[:,1:2].values
-
-length = training.shape[0]
-
-from sklearn.preprocessing import MinMaxScaler
-
+start = '2012-01-01'
+end = '2022-08-01'
+symbol = 'GOOG'
+offset = 30
+batch_size = 32
+epochs = 4
 scaler = MinMaxScaler(feature_range=(0,1))
 
-training_scaled = scaler.fit_transform(training)
+def train_test_split(data,train_size):
+    training_data_len = math.ceil(data.shape[0]*train_size)
 
-X_train = []
-y_train = []
-
-offset = 30
-for i in range(offset,length):
-    X_train.append(training_scaled[i-offset:i,0])
-    y_train.append(training_scaled[i,0])
+    train_data = data[0: training_data_len, :]
     
-X_train = np.array(X_train)
-y_train = np.array(y_train)
-X_train = np.reshape(X_train,(X_train.shape[0],X_train.shape[1],1))
+    x_train = []
+    y_train = []
 
-from keras.models import Sequential 
-from keras.layers import LSTM, Dropout, Dense
-regressor = Sequential()
-regressor.add(LSTM(units=5, return_sequences=True, 
-                   input_shape=(X_train.shape[1],1)))
-regressor.add(Dropout(0.2))
-regressor.add(LSTM(units=5, return_sequences=True))
-regressor.add(Dropout(0.2))
-regressor.add(LSTM(units=5, return_sequences=True))
-regressor.add(Dropout(0.2))
-regressor.add(LSTM(units=5, return_sequences=True))
-regressor.add(Dropout(0.2))
-regressor.add(Dense(units=1))
+    for i in range(offset, training_data_len):
+        x_train.append(train_data[i-offset:i, 0])
+        y_train.append(train_data[i, 0])
 
-regressor.compile(optimizer='adam', loss='mean_squared_error')
-regressor.fit(X_train, y_train, epochs=5, batch_size=32)
+    x_train, y_train = np.array(x_train), np.array(y_train)
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+    test_data = data[training_data_len-offset: , : ]
+
+    x_test = []
+    y_test = data[training_data_len:]
+    for i in range(training_data_len, len(data)):
+        x_test.append(data[i-offset:i, 0])
+
+    x_test = np.array(x_test)
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+    
+    return x_train, y_train, x_test, y_test
+
+
+def train(symbol, start, end, offset, batch_size, epochs, scaler):
+    stock_data = yf.download(symbol, start=start, end=end)
+
+    values = stock_data['Close'].values
+
+    scaled_data = scaler.fit_transform(values.reshape(-1,1))
+
+    x_train, y_train, _, _ = train_test_split(scaled_data,0.8)
+
+    model = keras.Sequential()
+    model.add(LSTM(100, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+    model.add(Dropout(0.2))
+    model.add(LSTM(100, return_sequences=False))
+    model.add(Dropout(0.2))
+    model.add(Dense(25))
+    model.add(Dense(1))
+
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs)
+
+    model_json = model.to_json()
+    with open("model.json", "w") as json_file:
+        json_file.write(model_json)
 
--- web-app.py
+++ web-app.py
@@ -6,3 +6,50 @@ Created on Tue Sep  6 20:35:25 2022
 @author: issacamara
 """
 
+import streamlit as st
+import pandas as pd
+import numpy as np
+import datetime
+import yfinance as yf
+
+
+st.title('Stock Price Prediction')
+st.markdown("<h1 style='text-align: center; color: red;'>Some title</h1>", 
+            unsafe_allow_html=True)
+
+hide_st_style = ("<style>" 
+                 # + "#MainMenu {visibility: hidden;}" 
+                 +"footer {visibility: hidden;}"
+                 # +"header {visibility: hidden;}"
+                 +"</style>"
+                )
+
+
+st.markdown(hide_st_style,unsafe_allow_html=True)
+
+
+sidebar = st.sidebar
+sidebar.write('Stock Settings')
+option = sidebar.selectbox('Select a stock',  ('GOOG', 'AAPL', 'TSLA'))
+sidebar.write('You selected:', option)
+
+col1, col2 = sidebar.columns(2, gap='medium')
+d1 = col1.date_input("From",datetime.date(2019, 7, 6))
+d2 = col2.date_input("To",datetime.date(2020, 7, 6))
+st.write('Your birthday is:', d1)
+
+_, col, _ = sidebar.columns(3, gap='small')
+
+if col.button('Forecast'):
+     st.write('Why hello there')
+     start = d1.strftime('%Y-%m-%d')
+     end = d2.strftime('%Y-%m-%d')
+     stock_data = yf.download('GOOG', start=start, end=end)
+     chart_data = pd.DataFrame(np.random.randn(50, 3),
+                               columns=["a", "b", "c"])
+
+     st.line_chart(stock_data['Close'])
+else:
+     st.write('Goodbye')
+
+
