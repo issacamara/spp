252ba110d501ef75dd738f6f7c19cca652abc6ab
issacamara <isskamara@live.fr>
2022-09-11 20:05:01 +0000

    Adding new files

---
+++ config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 11 15:01:34 2022
+
+@author: issacamara
+"""
+
+home_path = '/Users/issacamara/Developer/StockPricePredictor/spp-app/'
+models_path = 'models/'
+offset = 30
---
+++ helper.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 11 17:14:25 2022
+
+@author: issacamara
+"""
+import math
+import yfinance as yf
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler 
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from datetime import datetime, timedelta, date
+import time
+from sklearn.preprocessing import MinMaxScaler 
+import config as conf
+from tensorflow import keras
+
+
+today = date.today()
+delta = timedelta(days=2)
+symbol = 'GOOG'
+def forecast(stock_data, nb_days):
+    # yesterday = date.today() - timedelta(days=1)
+    # end = yesterday.strftime('%Y-%m-%d')
+    # start= (yesterday - timedelta(days=30)).strftime('%Y-%m-%d')
+    # stock_data = yf.download(symbol, start=start, end=end)
+    model = keras.models.load_model(conf.home_path + conf.models_path)
+    
+    scaler = MinMaxScaler(feature_range=(0,1))
+    
+    values = stock_data['Close'].values
+    scaled_data = scaler.fit_transform(values.reshape(-1,1))
+    
+    new_data = scaled_data.copy()
+    df = pd.DataFrame()
+    
+    offset = conf.offset
+    
+    last_day = max(stock_data.index)
+    for i in range(nb_days):
+        l = new_data.shape[0]
+        x_forecast = np.array([new_data[l-offset:l, 0]])
+        x_forecast = np.reshape(x_forecast, (x_forecast.shape[0], x_forecast.shape[1], 1))
+        forecast = model.predict(x_forecast)
+        new_data = np.append(new_data,forecast[0]).reshape(-1,1)
+        forecast = scaler.inverse_transform(forecast)
+        #print(forecast)
+        last_day = last_day + pd.Timedelta(1, unit="d")
+        df.loc[last_day,'Close'] = forecast[0,0]
+        
+    
+    df.index.names = ['Date']
+    return df
