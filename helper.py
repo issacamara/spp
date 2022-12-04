9ec885357ffbe8a84570b0dc26635a9537455dac
issacamara <isskamara@live.fr>
2022-12-04 21:30:35 +0000

    - Adding cufflinks package - Adding trading indicators

--- helper.py
+++ helper.py
@@ -61,7 +61,7 @@ models = {}
 for s in symbols:
     models[s] = keras.models.load_model(path_to_models + s)
 
-
+print("Write this everytime it runs -- Helper")
 # Computes predictors from data up to i-th item, i must be greater than offset
 def get_predictors(data, i):
     prev = data[i - offset:i, 0]
@@ -131,7 +131,7 @@ def get_stock_news(ticker, start, end):
         df = pd.DataFrame(data, columns=['Ticker', 'Exchange', 'News', 'Date'])
         res = df.groupby(['Ticker', 'Date']).agg({'News': lambda x: ' , '.join(set(x.dropna()))}).reset_index()
 
-        stock_prices = yf.download(symbol, start=start, end=end).reset_index()[['Date', 'Close']]
+        stock_prices = yf.download(symbol, start=start, end=end).reset_index()
         stock_prices['Date'] = stock_prices['Date'].astype(str)
         stock_prices['Ticker'] = ticker
         res = pd.merge(res, stock_prices, how='inner', left_on=['Date', 'Ticker'], right_on=['Date', 'Ticker'])
@@ -174,21 +174,94 @@ def get_all_stock_news(start, end):
         def custom_download(sym, s, e):
             df = yf.download(sym, s, e).reset_index()
             df['Ticker'] = sym
-            return df[['Date', 'Ticker', 'Close']]
+            df['Date'] = df['Date'].astype(str)
+            return df
 
         results = Parallel(n_jobs=num_cores)(delayed(custom_download)(t, start, end) for t in tickers)
         stock_prices = pd.concat(results, axis=0)
 
-        # stock_prices = yf.download(symbol, start=start, end=end).reset_index()[['Date', 'Close']]
-        stock_prices['Date'] = stock_prices['Date'].astype(str)
         res = pd.merge(res, stock_prices, how='inner', left_on=['Date', 'Ticker'], right_on=['Date', 'Ticker'])
         # res['Ticker'] = ticker
 
         return res
 
-start = date(2022, 11, 16)
-end = date(2022, 11, 17)
+
+def trading_indicators(df):
+    # SMA
+    df['SMA30'] = df['Close'].rolling(30).mean()
+    # CMA
+    df['CMA30'] = df['Close'].expanding().mean()
+    # EWMA
+    df['EWMA30'] = df['Close'].ewm(span=30).mean()
+
+
+    # Fibonacci Retracement
+    price_min = df.Close.min()
+    price_max = df.Close.max()
+    diff = price_max - price_min
+    level1 = price_max - 0.236 * diff
+    level2 = price_max - 0.382 * diff
+    level3 = price_max - 0.618 * diff
+
+
+
+def stochastic_oscillator(df):
+    # Stochastic Oscillator
+    """ A stochastic oscillator is an indicator that compares a specific closing price of an asset to a range of its
+    prices over time – showing momentum and trend strength. It uses a scale of 0 to 100. A reading below 20 generally
+    represents an oversold market and a reading above 80 an overbought market. However, if a strong trend is present,
+    a correction or rally will not necessarily ensue """
+    df['14-high'] = df['High'].rolling(14).max()
+    df['14-low'] = df['Low'].rolling(14).min()
+    df['%K'] = (df['Close'] - df['14-low']) * 100 / (df['14-high'] - df['14-low'])
+    df['%D'] = df['%K'].rolling(3).mean()
+    df = df.drop(columns=['14-high', '14-low'])
+    return df
+
+
+def macd(row):
+    exp1 = row['Close'].ewm(span=12, adjust=False).mean()
+    exp2 = row['Close'].ewm(span=26, adjust=False).mean()
+    row['MACD'] = exp1 - exp2
+    return row['MACD']
+
+def bollinger_bands(row):
+    # Bollinger Bands
+    row['TP'] = (row['Close'] + row['Low'] + row['High']) / 3
+    row['std'] = row['TP'].rolling(20).std(ddof=0)
+    row['MA-TP'] = row['TP'].rolling(20).mean()
+    row['BOLU'] = row['MA-TP'] + 2 * row['std']
+    row['BOLD'] = row['MA-TP'] - 2 * row['std']
+    return row[['BOLU', 'BOLD']]
+
+def rsi(row):
+    # RSI
+    delta = df['Close'].diff()
+    up = delta.clip(lower=0)
+    down = -1 * delta.clip(upper=0)
+    ema_up = up.ewm(com=13, adjust=False).mean()
+    ema_down = down.ewm(com=13, adjust=False).mean()
+    return ema_up / ema_down
+
+def atr(row):
+    # Average True Range
+    high_low = df['High'] - df['Low']
+    high_close = np.abs(df['High'] - df['Close'].shift())
+    low_close = np.abs(df['Low'] - df['Close'].shift())
+    ranges = pd.concat([high_low, high_close, low_close], axis=1)
+    true_range = np.max(ranges, axis=1)
+    return true_range.rolling(14).sum() / 14
+
+# import matplotlib.pyplot as plt
+# plt.style.use('default')
+# start = date(2021, 11, 16)
+# end = date(2022, 11, 17)
+# df = yf.download('TSLA',start,end)
+# df = stochastic_oscillator(df)
+# df['SMA30'] = df['Close'].rolling(30).mean()
+# df[['Close', 'SMA30']].plot(label='RELIANCE', figsize=(16, 8))
+
 # print('Done')
-data = get_stock_news('TSLA',start, end)
+# data = get_stock_news('TSLA', start, end)
 
 # scrape_google_finance(ticker="GOOGL:NASDAQ")
--- web-app.py
+++ web-app.py
@@ -10,13 +10,37 @@ import streamlit as st
 import pandas as pd
 import datetime as dt
 import yfinance as yf
+import cufflinks as cf
 import helper
 from dateutil.relativedelta import relativedelta  # to add days or years
 
+
 # model = keras.models.load_model(conf.home_path + conf.models_path)
 
+@st.cache
+def get_info(t):
+    dico = yf.Ticker(t).info
+    return {'shortName': dico['shortName'], 'logo_url': dico['logo_url']}
+
+
+@st.cache
+def get_all_tickers():
+    return list(pd.read_csv('tickers.csv', sep=';')['Symbol'])
+
+
+@st.cache
+def get_stock_prices(t, s, e):
+    return yf.download(t, start=s, end=e)
+
+
+def center_content(container, color, obj):
+    container.markdown(f"<h1 style='text-align: center; color: {color};'>{obj}</h1>", unsafe_allow_html=True)
+
+
+print("Write this everytime it runs -- WebApp", dt.datetime.now())
+
 title = 'Stock Price Prediction'
-st.markdown(f"<h1 style='text-align: center; color: black;'>{title}</h1>", unsafe_allow_html=True)
+# centerize(st,'black', title)
 # st.title('Stock Price Prediction')
 
 hide_st_style = ("<style>"
@@ -28,59 +52,85 @@ hide_st_style = ("<style>"
 
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
-sidebar = st.sidebar
-sidebar.write('Stock Settings')
-tickers = list(pd.read_csv('tickers.csv', sep=';')['Symbol'])
+tickers = get_all_tickers()
 
-stock = sidebar.selectbox('Select a stock', tickers)
 # temporality = sidebar.selectbox('Select the temporality', ('Minute', 'Day'))
 # sidebar.write('You selected:', stock)
-st.markdown(f"<h1 style='text-align: center; color: red;'>{stock} Stock Prices </h1>",
-            unsafe_allow_html=True)
-############################################### TABS ###############################################
-title1 = "History"
-title2 = "Forecast"
-# history_tab, forecast_tab = st.tabs([title1, title2])
-#
-# with history_tab:
-#    st.header(title1)
-#
-# with forecast_tab:
-#    st.header(title2)
 
-history_chart, forecast_chart = st.columns(2, gap='medium')
 ############################################### SIDEBAR ###############################################
+sidebar = st.sidebar
+center_content(sidebar, 'black', 'Stock Settings')
+# sidebar.write('Stock Settings')
+ticker = sidebar.selectbox('Select a stock', tickers)
 
-col1, col2 = sidebar.columns(2, gap='medium')
-d1 = col1.date_input("From", dt.date(2016, 7, 6))
-d2 = col2.date_input("To", dt.date(2020, 7, 6))
+from_col, to_col = sidebar.columns(2, gap='medium')
+d1 = from_col.date_input("From", dt.date(2016, 7, 6))
+d2 = to_col.date_input("To", dt.date(2020, 7, 6))
 investment = sidebar.number_input(label='Investment amount (€)', min_value=100, step=10)
 format1 = 'DD-MM-YYYY'
 format2 = '%Y-%m-%d'
 start_date = dt.datetime.now().date() - relativedelta(days=60)  # I need some range in the past
 end_date = dt.datetime.now().date() + relativedelta(days=60)
 max_days = end_date - start_date
-slider = sidebar.slider('Select a range', min_value=start_date, value=end_date, max_value=end_date, format=format1)
-
+slider = sidebar.slider('Select a range for forecasting', min_value=start_date, value=end_date,
+                        max_value=end_date, format=format1)
+info = get_info(ticker)
 _, col, _ = sidebar.columns(3, gap='small')
+logo = f"""<img src={info['logo_url']} alt='{ticker}' width='50' height='50'> """
 
+center_content(sidebar, 'black', logo)
+# sidebar.markdown(logo, unsafe_allow_html=True)
 
-# st.write(investment)
-st.markdown(f"<h1 style='text-align: center; color: blue;'>{investment} </h1>",
-            unsafe_allow_html=True)
+############################################### MAIN PANEL ###############################################
+center_content(st, 'red', f"""{info['shortName']}""")
+start = start_date.strftime(format2)
+end = end_date.strftime(format2)
+stock_data = get_stock_prices(ticker, start, end)
+qf = cf.QuantFig(stock_data, title="Quant Figure", legend='top', name='GS', up_color='green', down_color='red')
+    # qf.add_volume(name='Volume',up_color='green', down_color='red')
+    # qf.add_adx(periods=20)
+
+history_chart, forecast_chart = st.columns([5, 0.5], gap='large')
+
+ind_container = forecast_chart.container()
+ind_container.title("Indicators")
+sma = ind_container.checkbox('SMA')
+if(sma):
+    qf.add_sma(periods=20, legendgroup=True)
+
+ema = ind_container.checkbox('EMA')
+if(ema):
+    qf.add_ema(periods=20, name='EMA', color='pink', legendgroup=True)
+
+macd = ind_container.checkbox('MACD')
+if(macd):
+    qf.add_macd(fast_period=12, slow_period=26, signal_period=9, column=None, name='MACD',  color=['blue'])
+
+rsi = ind_container.checkbox('RSI')
+if(rsi):
+    qf.add_rsi(periods=20, color='java', name='RSI')
+
+adx = ind_container.checkbox('ADX')
+if(adx):
+    qf.add_adx(periods=20, name='ADX')
+
+atr = ind_container.checkbox('ATR')
+if(atr):
+    qf. add_atr(periods=20, name='ATR')
+bollinger_bands = ind_container.checkbox('Bollinger Bands')
+if(bollinger_bands):
+    qf.add_bollinger_bands(periods=20, boll_std=2, colors=['magenta', 'grey'], fill=True)
+
+fig = qf.iplot(asFigure=True)
+history_chart.plotly_chart(fig, use_container_width=True)
+
 if col.button('Forecast'):
-    start = start_date.strftime(format2)
-    end = end_date.strftime(format2)
-    stock_data = yf.download(stock, start=start, end=end)
-
     # st.write('Stock Prices between', start, 'and', end)
 
     # st.line_chart(stock_data['Close'])
-    predictions = helper.forecast(stock, stock_data, 60)
+    predictions = helper.forecast(ticker, stock_data, 60)
     df = pd.merge(left=stock_data, right=predictions, how='outer',
                   left_on='Date', right_on='Date')
     df = df.rename(columns={"Close_x": "Actual", "Close_y": "Prediction"})
-    history_chart.line_chart(df[['Actual']])
-    forecast_chart.line_chart(df[['Prediction']])
-
-
+    # history_chart.line_chart(df[['Actual']])
+    # forecast_chart.line_chart(df[['Prediction']])
